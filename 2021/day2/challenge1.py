937fd9682e6216a143ad949c013ca81e1256e894
DaniFojo <daniel.fojo@glovoapp.com>
2021-12-02 10:34:21 +0100

    Challenge 2 + refactor directories

---
+++ 2021/day2/challenge1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+input_file = Path(__file__).parent / "in.txt"
+with input_file.open("r") as f:
+    depth = 0
+    forward = 0
+    for line in f.readlines():
+        if line:
+            instruction, number = line.split(" ")[0], int(line.split(" ")[1])
+            if instruction == "forward":
+                forward += number
+            elif instruction == "down":
+                depth += number
+            else:
+                depth -= number
+
+
+print(depth * forward)
