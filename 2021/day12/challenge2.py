d6500e91705adab982baad4132db8d631028f388
Daniel Fojo <dani.fojo@gmail.com>
2021-12-12 08:40:11 +0100

    Day 12

---
+++ 2021/day12/challenge2.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from pathlib import Path
+
+input_file = Path(__file__).parent / "in.txt"
+
+caves = defaultdict(list)
+with input_file.open("r") as f:
+    for line in f.readlines():
+        s, e = line.split("-")
+        caves[s].append(e.strip("\n"))
+        caves[e.strip("\n")].append(s)
+
+
+def find_paths(current, used, twice_done):
+    res = 0
+    for cave in caves[current]:
+        if cave == "start":
+            continue
+        elif cave == "end":
+            res += 1
+        elif cave not in used:
+            used_copy = used.copy()
+            if cave == cave.lower():
+                used_copy.add(cave)
+            res += find_paths(cave, used_copy, twice_done)
+        elif cave in used and not twice_done:
+            used_copy = used.copy()
+            res += find_paths(cave, used_copy, True)
+    return res
+
+
+print(find_paths("start", set(["start"]), False))
