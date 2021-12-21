5acbc9a8f58e7205875a08ab5c1ca7dd29eaf0f0
Daniel Fojo <dani.fojo@gmail.com>
2021-12-21 11:25:37 +0100

    Day 21

---
+++ 2021/day21/challenge1.py
@@ -0,0 +1,41 @@
+from itertools import product
+from pathlib import Path
+
+input_file = Path(__file__).parent / "in.txt"
+
+with input_file.open("r") as f:
+    pos_1 = int(f.readline().split(":")[1])
+    pos_2 = int(f.readline().split(":")[1])
+
+
+class Die:
+    def __init__(self):
+        self.counter = -1
+        self.n = 0
+
+    def roll(self):
+        self.counter += 1
+        self.n += 1
+        self.counter %= 100
+        return self.counter + 1
+
+die = Die()
+points_1 = 0
+points_2 = 0
+pos_1 -= 1
+pos_2 -= 1
+
+while True:
+    pos_1 += die.roll() + die.roll() + die.roll()
+    pos_1 %= 10
+    points_1 += (pos_1 + 1)
+    if points_1 >= 1000:
+        print(die.n * points_2)
+        break
+
+    pos_2 += die.roll() + die.roll() + die.roll()
+    pos_2 %= 10
+    points_2 += (pos_2 + 1)
+    if points_2 >= 1000:
+        print(die.n * points_1)
+        break
---
+++ 2021/day21/challenge2.py
@@ -0,0 +1,32 @@
+from functools import lru_cache
+from itertools import product
+from pathlib import Path
+from collections import defaultdict
+
+input_file = Path(__file__).parent / "in.txt"
+with input_file.open("r") as f:
+    pos_1 = int(f.readline().split(":")[1])
+    pos_2 = int(f.readline().split(":")[1])
+
+probs = defaultdict(int)
+for values in product(range(1, 4), repeat=3):
+    probs[sum(values)] += 1
+
+@lru_cache(maxsize=None)
+def play(positions, points):
+
+    if points[1] >= 21:
+        return (0, 1)
+
+    total_wins = [0, 0]
+    for value, times in probs.items():
+        pos = ((positions[0] + value) % 10)
+        pts = points[0] + (pos + 1)
+        wins_this_turn = play((positions[1], pos), (points[1], pts))
+
+        total_wins[0] += wins_this_turn[1] * times
+        total_wins[1] += wins_this_turn[0] * times
+
+    return tuple(total_wins)
+
+print(max(play((pos_1 - 1, pos_2 - 1), (0, 0))))
